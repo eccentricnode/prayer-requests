bad7d1faab910f81d9b06522e8830cb79b8d33af
eccentricnode <shival.bigman@gmail.com>
2019-01-13 19:26:05 -0700

    added: server and configured start script

---
+++ personal/prayer-requests/server/server.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = 3200;
+
+server.use(middlewares);
+server.use(router);
+server.use(jsonServer.bodyParser);
+
+server.listen(port, () => console.log('JSON server is running port:', port));
